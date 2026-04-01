f540f62e8191b171de6c1e10c331976aa25e0c6d
Urtzi Ugarte <urtziugartte@gmail.com>
2026-04-01 23:14:52 +0200

    Mejoras y cambios en el front

--- frontend/src/app/pages/admin-salas/admin-salas.ts
+++ frontend/src/app/pages/admin-salas/admin-salas.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SalaService } from '../../services/sala';
 import { Sala } from '../../models/sala';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-admin-salas',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MatSnackBarModule],
   templateUrl: './admin-salas.html',
   styleUrl: './admin-salas.css'
 })
@@ -17,7 +19,7 @@ export class AdminSalasComponent implements OnInit {
   form!: FormGroup;
   editingId: number | null = null;
 
-  constructor(private service: SalaService, private fb: FormBuilder) {}
+  constructor(private service: SalaService, private fb: FormBuilder, private snackBar: MatSnackBar) {}
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -62,12 +64,15 @@ export class AdminSalasComponent implements OnInit {
   }
 
   delete(id: number) {
-    if (!confirm('¿Eliminar esta sala?')) return;
-    this.service.delete(id).subscribe({
-      next: () => this.load(),
-      error: err => console.error('Error eliminando', err)
-    });
-  }
+  if (!confirm('¿Eliminar esta sala?')) return;
+  this.service.delete(id).subscribe({
+    next: () => this.load(),
+    error: err => this.snackBar.open('Error al eliminar la sala', 'Cerrar', { duration: 3000, 
+  verticalPosition: 'top', 
+  horizontalPosition: 'center',
+  panelClass: ['snackbar-error'] })
+  });
+}
 
   reset() {
     this.editingId = null;
